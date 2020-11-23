c5f45acdfa3773220dbdb27f93899a36d1ba942a
nitish <nitishk12c@gmail.com>
2020-11-24 04:47:21 +0530

    State for For react hooks

---
+++ src/StateProvider.js
@@ -0,0 +1,16 @@
+import React, {createContext,useContext , useReducer} from "react";
+
+
+// preparing the data layer
+export const StateContext = createContext();
+
+export const StateProvider = ({initialState, reducer,children
+    }) => (
+        <StateContext.Provider value = {useReducer(reducer, 
+            initialState)}>
+                {children}
+        </StateContext.Provider>
+    );
+
+// hooks which allows us to pull information from the data layer
+export const useStateValue = () => useContext(StateContext);
